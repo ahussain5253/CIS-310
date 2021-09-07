9ace9ed925ca56f6ebf0c5ddf6b5e341b5199bfd
Ammar <ammarh786@gmail.com>
2021-09-07 11:41:22 -0400

    c

--- PROG0/PROG0/Source.cpp
+++ PROG0/PROG0/Source.cpp
@@ -4,20 +4,16 @@ using namespace std;
 
 int decimalToBinary(int decimalNum) {
 
-	int binaryNum = decimalNum % 2;
+	int binaryNUM = decimalNum % 2;
 
-	return binaryNum;
-
+	int binary[9];
+	
 }
 
 int main() {
 
 
-	for (int i = 0; i < 255; i++) {
 
-		cout << decimalToBinary(i) << endl;
-
-	}
 	
 
 
