803b4fd7e43ebe9d5eb833efda8e1d2a74ac8f73
Ammar <ammarh786@gmail.com>
2021-10-21 15:01:53 -0400

    ch

--- AssemblyTesting/AssemblyTesting/Testing.asm
+++ AssemblyTesting/AssemblyTesting/Testing.asm
@@ -3,29 +3,12 @@
 INCLUDE Irvine32.inc
 
 .data
-	prompt byte "Please enter a number from 0 - 100: "
-    decNum BYTE ?
-	grade BYTE ?
-   
 
-
+  
 .code
 main PROC
 
-	mov edx, offset prompt
-	call writestring
 
-	call readint
-	mov decNum, al
-
-	.IF decNUM > 90
-		mov al, 'A'
-	.ENDIF
-
-
-
-
-	
 
 	INVOKE ExitProcess, 0
 main ENDP
